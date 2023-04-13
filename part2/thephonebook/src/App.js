e36f5cd9d38e7b9a206cf61bc3839afce82fb6c1
Sandi Cej <cej.sandi@gmail.com>
2023-04-13 21:48:01 +0200

    part2/thephonebook: step1 development

---
+++ part2/thephonebook/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = ({ target }) => {
+    setNewName(target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      {persons.map(({ name }) => <div key={name}>{name}</div>)}
+    </div>
+  )
+}
+
+export default App
